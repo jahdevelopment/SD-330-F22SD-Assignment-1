ae82e4720a6d925c35a170ae28ae60e11388a6a8
juanhernandezdeveloper <jualherac@gmail.com>
2023-03-09 23:26:08 -0600

    Add Models with its properties

---
+++ Models/Song.cs
@@ -0,0 +1,33 @@
+﻿namespace SD_330_F22SD_Assignment_1.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int DurationSeconds { get; set; }
+
+        public Album Album { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public virtual HashSet<PlaylistSong> PlaylistSongs { get; set; } = new HashSet<PlaylistSong>();
+
+        public Song()
+        {
+
+        }
+
+        public Song(string title, int durationSeconds, Album album)
+        {
+            Title = title;
+            
+            DurationSeconds = durationSeconds;
+            
+            Album = album;
+
+            AlbumId = album.Id;
+        }
+    }
+}
