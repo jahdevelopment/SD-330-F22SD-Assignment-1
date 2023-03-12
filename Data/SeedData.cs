e2e7865dcea70e11e2f612a2df59bbe7d6a6408c
juanhernandezdeveloper <jualherac@gmail.com>
2023-03-11 23:08:39 -0600

    Configuration dependency injection

---
+++ Data/SeedData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SD_330_F22SD_Assignment_1.Data
+{
+    public static class SeedData
+    {
+        public async static Task Initialize(IServiceProvider serviceProvider)
+        {
+            SpotifyContext context = new SpotifyContext (serviceProvider.GetRequiredService<DbContextOptions<SpotifyContext>>());
+
+
+        }
+    }
+}
